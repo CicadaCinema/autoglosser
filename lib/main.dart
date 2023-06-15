a2316a8c7c6cbea544137c00f14d9f948987b54a
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2023-06-15 20:12:07 +0100

    add basic demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
-import 'package:chicago/chicago.dart';
-import 'package:flutter/widgets.dart';
+import 'package:autoglosser/src/widgets/text_display.dart';
+import 'package:flutter/material.dart';
+
+import 'src/data_structures.dart' as ag;
+
+final sample = ag.Text.fromString('''知天之所為，知人之所為者，至矣。
+知天之所為者，天而生也；知人之所為者，以其知之所知，以養其知之所不知，終其天年而不中道夭者，是知之盛也。
+雖然，有患。夫知有所待而後當，其所待者特未定也。
+庸詎知吾所謂天之非人乎？所謂人之非天乎？且有真人，而後有真知。''');
 
 void main() {
   runApp(const MyApp());
@@ -10,54 +17,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ChicagoApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          const Flexible(child: Placeholder()),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-              ),
-              PushButton(
-                onPressed: _incrementCounter,
-                label: 'Increment',
-              ),
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 1,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: 'Translation'),
+              ],
+            ),
+            // Do not show app bar, only show tabs.
+            toolbarHeight: 0,
+          ),
+          body: TabBarView(
+            children: [
+              TextDisplay(text: sample),
             ],
           ),
-          const Flexible(child: Placeholder()),
-        ],
+        ),
       ),
     );
   }
---
+++ lib/src/widgets/text_display.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+import '../data_structures.dart' as ag;
+
+const _compactDecoration = InputDecoration(
+  isDense: true,
+  contentPadding: EdgeInsets.all(5),
+  border: OutlineInputBorder(),
+);
+
+/// Displays a source word, its pronounciation and its gloss, allowing the
+/// pronounciation and gloss to be edited while hovering the mouse over this
+/// widget.
+class WordDisplay extends StatefulWidget {
+  const WordDisplay({super.key, required this.word});
+
+  final ag.Word word;
+
+  @override
+  State<WordDisplay> createState() => _WordDisplayState();
+}
+
+class _WordDisplayState extends State<WordDisplay> {
+  bool _textFieldsVisible = false;
+  final _pronounciationController = TextEditingController();
+  final _glossController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    // Initially, set the text field values to the existing pronounciation and gloss strings.
+    _pronounciationController.text = widget.word.pronounciation;
+    _glossController.text = widget.word.gloss;
+
+    // Add listeners to update these strings whenever the text field values change.
+    _pronounciationController.addListener(() {
+      widget.word.pronounciation = _pronounciationController.text;
+    });
+    _glossController.addListener(() {
+      widget.word.gloss = _glossController.text;
+    });
+  }
+
+  @override
+  void dispose() {
+    _pronounciationController.dispose();
+    _glossController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (event) {
+        setState(() {
+          _textFieldsVisible = true;
+        });
+      },
+      onExit: (event) {
+        setState(() {
+          _textFieldsVisible = false;
+        });
+      },
+      child: Column(children: [
+        Text(widget.word.source),
+        _textFieldsVisible
+            ? SizedBox(
+                width: 50,
+                child: TextField(
+                  controller: _pronounciationController,
+                  decoration: _compactDecoration,
+                ),
+              )
+            : Text(widget.word.pronounciation),
+        _textFieldsVisible
+            ? SizedBox(
+                width: 50,
+                child: TextField(
+                  controller: _glossController,
+                  decoration: _compactDecoration,
+                ),
+              )
+            : Text(widget.word.gloss),
+      ]),
+    );
+  }
+}
+
+class LineDisplay extends StatefulWidget {
+  const LineDisplay({super.key, required this.line});
+
+  final ag.Line line;
+
+  @override
+  State<LineDisplay> createState() => _LineDisplayState();
+}
+
+class _LineDisplayState extends State<LineDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: widget.line.words.map((w) => WordDisplay(word: w)).toList(),
+    );
+  }
+}
+
+class ChunkDisplay extends StatefulWidget {
+  const ChunkDisplay({super.key, required this.chunk});
+
+  final ag.Chunk chunk;
+
+  @override
+  State<ChunkDisplay> createState() => _ChunkDisplayState();
+}
+
+class _ChunkDisplayState extends State<ChunkDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ...widget.chunk.lines.map((l) => LineDisplay(line: l)),
+        Text(widget.chunk.translation),
+      ],
+    );
+  }
+}
+
+class TextDisplay extends StatefulWidget {
+  const TextDisplay({super.key, required this.text});
+
+  final ag.Text text;
+
+  @override
+  State<TextDisplay> createState() => _TextDisplayState();
+}
+
+class _TextDisplayState extends State<TextDisplay> {
+  // The SizedBox widgets add extra padding at the top and bottom of the list.
+  late final _chunkDisplayWidgets = [
+    const SizedBox(height: 50),
+    ...widget.text.chunks.map((c) => ChunkDisplay(chunk: c)),
+    const SizedBox(height: 50),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    // Display the chunks in a lazy list.
+    return ListView.builder(
+      itemCount: _chunkDisplayWidgets.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: _chunkDisplayWidgets[index],
+        );
+      },
+    );
+  }
+}
