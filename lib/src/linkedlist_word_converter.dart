8a4a1b45ce3214b207ab15fda80538d4ea79eaf7
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2023-07-13 18:06:58 +0100

    fix: relax type constraints on custom `JsonConverter` and instead cast objects to the proper type as needed

--- lib/src/linkedlist_word_converter.dart
+++ lib/src/linkedlist_word_converter.dart
@@ -4,20 +4,19 @@ import 'package:json_annotation/json_annotation.dart';
 
 import 'data_structures.dart';
 
-class LinkedListWordConverter
-    extends JsonConverter<LinkedList<Word>, List<Map<String, dynamic>>> {
+class LinkedListWordConverter extends JsonConverter<LinkedList<Word>, List> {
   const LinkedListWordConverter();
 
   @override
-  LinkedList<Word> fromJson(List<Map<String, dynamic>> json) {
+  LinkedList<Word> fromJson(List json) {
     final linkedList = LinkedList<Word>();
-    final words = json
-        .map((Map<String, dynamic> encodedWord) => Word.fromJson(encodedWord));
+    final words = json.map(
+        (encodedWord) => Word.fromJson(encodedWord as Map<String, dynamic>));
     linkedList.addAll(words);
     return linkedList;
   }
 
   @override
-  List<Map<String, dynamic>> toJson(LinkedList<Word> object) =>
+  List toJson(LinkedList<Word> object) =>
       object.map((Word word) => word.toJson()).toList();
 }
